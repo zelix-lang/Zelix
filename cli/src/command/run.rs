c004a41598ec80a75daa6cb41573791a5c9322a1
rodri-r-z <2nw53j32643y@gmail.com>
2024-11-17 12:09:28 -0500

    Start implementation of run command

--- cli/src/command/run.rs
+++ cli/src/command/run.rs
@@ -1,15 +1,18 @@
-use std::{path::PathBuf, process::exit};
+use std::{fs::read_to_string, path::PathBuf, process::exit};
 
-use shared::{logger::{Logger, LoggerImpl}, result::try_unwrap};
-use std::{env::current_dir, fs::exists};
+use lexer::{Lexer, LexerImpl};
+use shared::{logger::{Logger, LoggerImpl}, path::retrieve_path, result::try_unwrap};
+use std::{env::current_dir, fs::{exists, metadata}};
+
+use crate::structs::surf_config_file::SurfConfigFile;
 
 pub fn run_command(path: Option<PathBuf>) {
-    let final_path = path.unwrap_or(
+    let final_path = retrieve_path(path.unwrap_or(
         try_unwrap(
             current_dir(), 
             "Failed to get current directory"
         )
-    );
+    ));
 
     if !try_unwrap(
         exists(final_path.clone()),
@@ -22,4 +25,71 @@ pub fn run_command(path: Option<PathBuf>) {
         );
         exit(1);
     }
+
+    if !try_unwrap(
+        metadata(final_path.clone()),
+        "Failed to get metadata from the path"
+    ).is_dir() {
+        Logger::err(
+            &"The path is not a directory!",
+            &[
+                &"Make sure the path is correct",
+                &"Start a new project with surf init"
+            ],
+            &[&"No trace available"]
+        );
+        exit(1);
+    }
+
+    let config_file_path = final_path.join("Surf.yml");
+
+    if !config_file_path.exists() {
+        Logger::err(
+            &"The path is not a Surf project!",
+            &[
+                &"Make sure the path is correct",
+                &"Start a new project with surf init"
+            ],
+            &[&"No trace available"]
+        );
+        exit(1);
+    }
+
+    let config_file : SurfConfigFile = try_unwrap(
+        serde_yml::from_str(
+            try_unwrap(
+                read_to_string(config_file_path),
+                "Failed to read the Surf.yml file"
+            ).as_str()
+        ),
+        "Failed to parse the Surf.yml file"
+    );
+
+    let main_file = final_path.join(config_file.main_file);
+
+    if !main_file.exists() {
+        Logger::err(
+            &"The main file doesn't exist!",
+            &[
+                &"Make sure the path is correct",
+                &"Start a new project with surf init"
+            ],
+            &[&"No trace available"]
+        );
+        exit(1);
+    }
+
+    let main_file_content = try_unwrap(
+        read_to_string(main_file.clone()),
+        "Failed to read the main file"
+    );
+
+    let mut lexer = Lexer::new();
+    let tokens = lexer.tokenize(
+        &main_file_content,
+        &main_file.to_str().unwrap().to_string()
+    );
+
+    println!("{:?}", tokens);
+
 }
