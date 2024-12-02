8f598ca6f9f31905672ef1dce68f6f21cf4987fa
rodri-r-z <2nw53j32643y@gmail.com>
2024-12-01 21:03:55 -0500

    Remove no-longer-required comments

--- checker/src/lib.rs
+++ checker/src/lib.rs
@@ -3,6 +3,7 @@ mod main_function_checker;
 mod scope_checker;
 mod header_checker;
 mod variable_checker;
+mod types;
 
 use function_checker::analyze_functions;
 use main_function_checker::check_main_function;
@@ -12,9 +13,6 @@ use shared::code::file_code::{FileCode, FileCodeImpl};
 
 pub fn analyze(code: &FileCode) {
 
-    // Create a Clang instance and an Index to parse .h and .hpp files
-    // in order to determine non-native functions and types
-    // which are needed for static analysis
     analyze_functions(code.get_functions(), code.get_source());
     check_main_function(code.get_functions());
     analyze_scope(code);
