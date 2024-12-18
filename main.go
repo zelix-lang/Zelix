0cb483af3a5d1429d8eb6bef08527a79eead45f9
Rodrigo R. <2nw53j32643y@gmail.com>
2024-12-17 22:14:01 -0500

    Remove unused comments

--- main.go
+++ main.go
@@ -7,36 +7,6 @@ import (
 )
 
 func main() {
-	/*file := os.Getenv("SURF_DUMMY_FILE")
-
-	// Read the file
-	input, err := os.ReadFile(file)
-
-	if err != nil {
-		panic(err)
-	}
-
-	start := time.Now()
-
-	// Lex the input
-	tokens := lexer.Lex(string(input), file)
-
-	fmt.Println("Lexed in:", time.Since(start))
-
-	start = time.Now()
-	// Parse the tokens into a FileCode
-	fileCode := ast.Parse(tokens)
-	fmt.Println("Wrapped to AST in:", time.Since(start))
-
-	start = time.Now()
-	// Analyze the file code
-	checker.AnalyzeFileCode(*fileCode, file)
-	fmt.Println("Analyzed in:", time.Since(start))
-
-	start = time.Now()
-	// Interpret the file code
-	core.Interpret(fileCode, file)
-	fmt.Println("Interpreted in:", time.Since(start))*/
 	app := &cli.App{
 		Name:  "surf",
 		Usage: "A blazingly fast programming language",
