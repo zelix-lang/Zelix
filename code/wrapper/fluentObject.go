41843722b623124c62c62e2e69308c9455b7d5ac
Rodrigo R. <2nw53j32643y@gmail.com>
2024-12-26 18:05:29 -0500

    Add documentation to fluentObject.go

--- code/wrapper/fluentObject.go
+++ code/wrapper/fluentObject.go
@@ -1,10 +1,19 @@
 package wrapper
 
+// FluentObject is a struct that wraps a type and a value.
 type FluentObject struct {
-	_type TypeWrapper
-	value interface{}
+	_type TypeWrapper // _type represents the type of the wrapped value.
+	value interface{} // value holds the actual value being wrapped.
 }
 
+// NewFluentObject creates a new FluentObject with the given type and value.
+// Parameters:
+//   - _type: The type of the value being wrapped.
+//   - value: The actual value to be wrapped.
+//
+// Returns:
+//
+//	A new instance of FluentObject.
 func NewFluentObject(_type TypeWrapper, value any) FluentObject {
 	return FluentObject{
 		_type: _type,
@@ -12,10 +21,18 @@ func NewFluentObject(_type TypeWrapper, value any) FluentObject {
 	}
 }
 
+// GetType returns the type of the wrapped value.
+// Returns:
+//
+//	The type of the wrapped value.
 func (so *FluentObject) GetType() TypeWrapper {
 	return so._type
 }
 
+// GetValue returns the wrapped value.
+// Returns:
+//
+//	The wrapped value.
 func (so *FluentObject) GetValue() any {
 	return so.value
 }
