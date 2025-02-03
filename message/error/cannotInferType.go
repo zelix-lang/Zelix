4c863c68cce5d56fea3a1a67bbd38412fa951447
rodri-r-z <2nw53j32643y@gmail.com>
2025-02-03 12:52:20 -0500

    Add error E0011

---
+++ message/error/cannotInferType.go
@@ -0,0 +1,30 @@
+/*
+   The Fluent Programming Language
+   -----------------------------------------------------
+   Copyright (c) 2025 Rodrigo R. & All Fluent Contributors
+   This program comes with ABSOLUTELY NO WARRANTY.
+   For details type `fluent -l`. This is free software,
+   and you are welcome to redistribute it under certain
+   conditions; type `fluent -l -f` for details.
+*/
+
+package error
+
+import "fluent/logger"
+
+// CannotInferType logs an error message indicating that the type of one object cannot be inferred.
+// It provides help and additional information for the user to understand the issue.
+func CannotInferType() {
+	logger.Error("Cannot infer this object's type")
+	logger.Help(
+		"Fluent cannot infer the type of this object.",
+		"Type inferring works for assignments, parameters and declarations",
+		"Statements like [1, 2, 3] cannot have their type inferred because",
+		"at such point, the Fluent compiler does not expect a type to infer or match",
+	)
+	logger.Info(
+		"For more information, refer to:",
+		"https://fluent-lang.github.io/book/codes/E0011",
+		"Full details:",
+	)
+}
