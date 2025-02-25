f51bbcfb52aff5c84f5777d99d0fd70c41d9d393
rodrigo <2nw53j32643y@gmail.com>
2025-02-25 09:49:06 -0500

    Let fluentc handle LLVM/Linker detection

--- cli/buildCommand.go
+++ cli/buildCommand.go
@@ -36,24 +36,7 @@ var includePathPOSIX = fmt.Sprintf("%s/posix", includePath)
 var includePathWin = fmt.Sprintf("%s/win", includePath)
 var fluentExtensionRegex = regexp.MustCompile("\\.fluent$")
 var isWindows = runtime.GOOS == "windows"
-var isLinux = runtime.GOOS == "linux"
-var isMac = runtime.GOOS == "darwin"
 var isPOSIX = !isWindows
-
-func showLLVMNotInstalled() {
-	logger.Error("LLVM is not installed.")
-	logger.Info(
-		"Please install it by downloading the necessary",
-		"binaries from the official LLVM repository.",
-	)
-	logger.Help("Make sure you have installed:")
-	logger.Help("  - llc")
-	logger.Help("  - llvm-link")
-	logger.Help("  - lld")
-	logger.Help("  - llvm-as")
-	logger.Help("You might need to install lld separately.")
-	os.Exit(1)
-}
 
 // BuildCommand compiles the given Fluent project into an executable
 func BuildCommand(context *cli.Command) {
@@ -70,46 +53,6 @@ func BuildCommand(context *cli.Command) {
 			"binaries from the official repository.",
 		)
 		os.Exit(1)
-	}
-
-	// Also check if the necessary LLVM binaries are installed
-	fmt.Println(ansi.Colorize(ansi.BoldBrightYellow, "⚠️ Checking if LLVM is installed...."))
-
-	commands := []string{
-		"llc",
-		"llvm-link",
-		"llvm-as",
-	}
-
-	for _, command := range commands {
-		cmd = exec.Command(command, "--version")
-		err = cmd.Run()
-
-		if err != nil {
-			showLLVMNotInstalled()
-		}
-	}
-
-	// Also check for lld
-	var systemLinker string
-	if isWindows {
-		systemLinker = "lld-link"
-	} else if isMac {
-		systemLinker = "ld64.lld"
-	} else if isLinux {
-		// Use the GNU linker instead of the LLVM linker
-		// As it comes by default in most Linux distributions
-		systemLinker = "ld"
-	} else {
-		// We are most likely in a BSD system
-		// Use the LLVM linker
-		systemLinker = "ld.lld"
-	}
-
-	cmd = exec.Command(systemLinker, "--version")
-	err = cmd.Run()
-	if err != nil {
-		showLLVMNotInstalled()
 	}
 
 	fileCodes, fileCodesMap := CheckCommand(context)
@@ -262,11 +205,11 @@ func BuildCommand(context *cli.Command) {
 		os.Exit(1)
 	}
 
-	fmt.Println(ansi.Colorize(ansi.BoldBrightYellow, "⚠️ Invoking fluentc backend...."))
+	fmt.Println(ansi.Colorize(ansi.BoldBrightYellow, "⚠️ Invoking fluent backend...."))
 	fmt.Println(ansi.Colorize(ansi.BrightBlack, "⚠️ The output you will see from now on is coming from the fluentc command."))
 
 	// Invoke the fluentc backend
-	cmd = exec.Command("fluentc", "-l", systemLinker, "-o", outPath, globalIrPath)
+	cmd = exec.Command("fluentc", "-o", outPath, globalIrPath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
