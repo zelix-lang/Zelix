6be47947ed71833ab4e103f17bf3d45fe2974382
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-15 08:16:09 -0500

    Update representation string to include 'end_co' for module construction

--- ir/rule/object/objectCreation.go
+++ ir/rule/object/objectCreation.go
@@ -271,7 +271,7 @@ func MarshalObjectCreation(
 			// representation to avoid the call to the constructor
 			// appearing before the instruction that constructs
 			// the module
-			element.Parent.Representation.WriteString("\n")
+			element.Parent.Representation.WriteString("end_co\n")
 
 			// Insert a call to the constructor
 			element.Parent.Representation.WriteString("c ")
@@ -323,6 +323,8 @@ func MarshalObjectCreation(
 				colAddress,
 				fileAddress,
 			)
+		} else {
+			element.Parent.Representation.WriteString("end_co")
 		}
 	}
 
