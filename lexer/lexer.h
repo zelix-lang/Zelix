5e1b1b755533ab3e6611487130cebfb0ffe7b45e
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-31 17:02:32 -0500

    Add support for block comments in lexer

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -148,6 +148,14 @@ static inline pair_lex_result_t lexer_tokenize(
             continue;
         }
 
+        if (c == '/' && source[i + 1] == '*')
+        {
+            // Block comment start
+            in_block_comment = TRUE;
+            i++; // Skip the next character
+            continue;
+        }
+
         // Write the current character to the string builder
         write_char_string_builder(&current, c);
 
