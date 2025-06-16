4f2b85164ac09cec6e2f30917c524c174d4bf8a4
rodrigoo-r <2nw53j32643y@gmail.com>
2025-06-16 12:26:06 -0500

    Add license information and copyright notice to block.h

--- parser/rule/block.h
+++ parser/rule/block.h
@@ -1,3 +1,17 @@
+/*
+    The Fluent Programming Language
+    -----------------------------------------------------
+    This code is released under the GNU GPL v3 license.
+    For more information, please visit:
+    https://www.gnu.org/licenses/gpl-3.0.html
+    -----------------------------------------------------
+    Copyright (c) 2025 Rodrigo R. & All Fluent Contributors
+    This program comes with ABSOLUTELY NO WARRANTY.
+    For details type `fluent l`. This is free software,
+    and you are welcome to redistribute it under certain
+    conditions; type `fluent l -f` for details.
+*/
+
 //
 // Created by rodrigo on 6/16/25.
 //
