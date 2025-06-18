d0291c9e093cddfc5f66df024ff03d5b3b6e2ec1
rodrigoo-r <2nw53j32643y@gmail.com>
2025-06-18 10:45:36 -0500

    Fix object creation parsing to skip 'new' token in expression.h

--- parser/rule/expression.h
+++ parser/rule/expression.h
@@ -271,7 +271,7 @@ static inline bool parse_expression(
                 }
 
                 // Update the start counter
-                start += obj_creation_len;
+                start += obj_creation_len + 1; // +1 to skip the 'new' token
                 is_arithmetic = FALSE;
                 is_prop_access = TRUE;
             }
