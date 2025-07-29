d897b89a97044714d4b5fc3b973b266f489d1189
rodrigoo-r <2nw53j32643y@gmail.com>
2025-07-29 15:15:12 -0500

    Add external_string class for managing external character buffers

---
+++ container/external_string.h
@@ -0,0 +1,60 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 7/29/25.
+//
+
+#pragma once
+#include "except/exception.h"
+
+namespace fluent::container
+{
+    class external_string
+    {
+        char *buffer;
+        size_t len = 0;
+
+    public:
+        external_string(char *buffer, const size_t len)
+            : buffer(buffer), len(len)
+        {
+            if (buffer == nullptr || len == 0)
+            {
+                throw except::exception("Buffer cannot be null or length zero");
+            }
+        }
+
+        [[nodiscard]] char *ptr()
+        const {
+            return buffer;
+        }
+
+        [[nodiscard]] size_t size()
+        const {
+            return len;
+        }
+    };
+}
