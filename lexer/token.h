a1e7ab9ae975f323a0353393a86a7195b17e2994
rodrigoo-r <2nw53j32643y@gmail.com>
2025-07-30 20:04:24 -0500

    Add token structure and enumeration for the Fluent programming language

---
+++ lexer/token.h
@@ -0,0 +1,78 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 7/30/25.
+//
+
+#pragma once
+#include <optional>
+
+#include "fluent/container/external_string.h"
+
+namespace fluent::lexer
+{
+    struct token
+    {
+        enum t_type
+        {
+            UNKNOWN,
+            IMPORT,
+            FUNCTION,
+            MOD,
+            ARROW,
+            OPEN_CURLY,
+            CLOSE_CURLY,
+            OPEN_PAREN,
+            CLOSE_PAREN,
+            OPEN_BRACKET,
+            CLOSE_BRACKET,
+            IDENTIFIER,
+            STRING,
+            NUMBER,
+            DECIMAL,
+            NOTHING,
+            STRING_LITERAL,
+            NUMBER_LITERAL,
+            DECIMAL_LITERAL,
+            SEMICOLON,
+            COMMA,
+            COLON,
+            EQUALS,
+            PLUS,
+            MINUS,
+            MULTIPLY,
+            DIVIDE,
+            AND,
+            OR,
+            NOT,
+        };
+
+        std::optional<container::external_string> value;
+        t_type type = UNKNOWN;
+        size_t line = 0;
+        size_t column = 0;
+    };
+}
