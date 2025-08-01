0af280c472dd76f49dbd6c82cf371a6c314652dc
rodrigoo-r <2nw53j32643y@gmail.com>
2025-08-01 16:13:28 -0500

    Add parser implementation for AST construction and error handling

---
+++ parser/parser.cpp
@@ -0,0 +1,64 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+#include "parser.h"
+using namespace fluent;
+
+parser::ast parse(container::stream<lexer::token> &tokens)
+{
+    // Create the root AST node
+    parser::ast root;
+
+    bool top_level = true; // Flag to track if we are at the top level of the AST
+
+    // Iterate over the tokens
+    auto current_opt = tokens.next();
+    while (current_opt.is_some())
+    {
+        switch (
+            const auto &current = current_opt.get();
+            current.type
+        )
+        {
+            case lexer::token::IMPORT:
+            {
+                // TODO!
+                break;
+            }
+
+            default:
+            {
+                parser::global_err.type = parser::UNEXPECTED_TOKEN;
+                parser::global_err.line = current.line;
+                parser::global_err.column = current.column;
+                return root;
+            }
+        }
+
+        current_opt = tokens.next();
+    }
+
+    return root;
+}
