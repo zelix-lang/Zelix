16977be54f311fdc46e8cc8c0d2c2854fde49c20
rodrigoo-r <2nw53j32643y@gmail.com>
2025-08-01 17:21:22 -0500

    Add import function implementation for AST node handling

---
+++ parser/import/import.h
@@ -0,0 +1,67 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 8/1/25.
+//
+
+#pragma once
+#include "fluent/container/stream.h"
+#include "lexer/token.h"
+#include "parser/expect.h"
+#include "parser/parser.h"
+
+namespace fluent::parser::rule
+{
+    inline void imp(
+        ast &root,
+        container::stream<lexer::token> &tokens,
+        const bool &top_level,
+        const lexer::token &trace
+    )
+    {
+        // Make sure we are at the top level
+        if (!top_level)
+        {
+            global_err.type = ILLEGAL_IMPORT;
+            global_err.column = trace.column;
+            global_err.line = trace.line;
+            return;
+        }
+
+        expect(tokens, lexer::token::STRING_LITERAL);
+        const auto path = tokens.next()
+            .get();
+
+        // Expect a semicolon
+        expect(tokens, lexer::token::SEMICOLON);
+
+        // Create the import node
+        ast import_node;
+        import_node.rule = ast::IMPORT;
+        import_node.value = path.value;
+        root.children.push_back(import_node);
+    }
+}
