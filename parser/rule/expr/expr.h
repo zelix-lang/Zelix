442241359b4dd735d944c0ddb927f19c63b2804e
rodrigoo-r <2nw53j32643y@gmail.com>
2025-08-02 12:30:46 -0500

    Add expression handling in expr.h for AST node management

---
+++ parser/rule/expr/expr.h
@@ -0,0 +1,269 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 8/1/25.
+//
+
+#pragma once
+#include "fluent/container/stream.h"
+#include "lexer/token.h"
+#include "memory/allocator.h"
+#include "parser/parser.h"
+#include "parser/rule/extractor.h"
+#include "queue.h"
+#include "likely.h"
+
+namespace fluent::parser::rule
+{
+    inline void expression(
+        ast *&root,
+        container::stream<lexer::token> &tokens,
+        memory::lazy_allocator<ast> &allocator,
+        const lexer::token &trace
+    )
+    {
+        // Create a new AST node for the expression
+        ast *expr_node = allocator.alloc();
+        expr_node->rule = ast::EXPRESSION;
+        root->children.push_back(expr_node);
+
+        // Extract all tokens until the next semicolon
+        auto expr_group = extract(
+            tokens,
+            lexer::token::SEMICOLON,
+            lexer::token::UNKNOWN,
+            false, // Do not handle nested delimiters
+            false // Do not exclude the first delimiter
+        );
+
+        // Create a queue for nested expressions
+        container::vector<expr::queue_node> expr_queue;
+        expr_queue.emplace_back(expr_group, expr_node, root);
+
+        // Process all expressions
+        while (!expr_queue.empty())
+        {
+            auto &[expr_stream, node, parent] = expr_queue.ref_at(expr_queue.size() - 1);
+            expr_queue.pop_back(); // Remove the current expression from the queue
+
+            auto first_opt = expr_stream.next();
+            // Check if the current element is empty
+            if (first_opt.is_none())
+            {
+                global_err.type = UNEXPECTED_TOKEN;
+                global_err.column = trace.column;
+                global_err.line = trace.line;
+                throw except::exception("Unexpected empty expression");
+            }
+
+
+            // Get the first token to determine the expression type
+            ast *candidate = nullptr;
+            // Define the likely operations
+#       if defined(__x86_64__) || defined(_M_X64) || defined(__aarch64__)
+            uint64_t likely = 0;
+#       else
+            uint32_t likely = 0;
+#       endif
+
+            auto &first = first_opt.get();
+            switch (first.type)
+            {
+                case lexer::token::IDENTIFIER:
+                {
+                    likely |= expr::ALL_LIKELY; // All operations are likely
+
+                    // Update the candidate
+                    candidate = allocator.alloc();
+                    candidate->rule = ast::IDENTIFIER;
+                    candidate->value = first.value;
+                    break;
+                }
+
+                case lexer::token::NUMBER_LITERAL:
+                {
+                    // Arithmetic operation is likely
+                    likely |= expr::ARITHMETIC_OP_LIKELY
+                           | expr::BOOLEAN_OP_LIKELY; // Boolean ops are also likely (ex. 1 == 2)
+
+                    // Update the candidate
+                    candidate = allocator.alloc();
+                    candidate->rule = ast::NUMBER_LITERAL;
+                    candidate->value = first.value;
+                    break;
+                }
+
+                case lexer::token::DECIMAL_LITERAL:
+                {
+                    // Arithmetic operation is likely
+                    likely |= expr::ARITHMETIC_OP_LIKELY
+                           | expr::BOOLEAN_OP_LIKELY; // Boolean ops are also likely (ex. 1 == 2)
+
+                    // Update the candidate
+                    candidate = allocator.alloc();
+                    candidate->rule = ast::DECIMAL_LITERAL;
+                    candidate->value = first.value;
+                    break;
+                }
+
+                case lexer::token::STRING_LITERAL:
+                {
+                    // Boolean operation is likely
+                    likely |= expr::BOOLEAN_OP_LIKELY;
+
+                    // Update the candidate
+                    candidate = allocator.alloc();
+                    candidate->rule = ast::STRING_LITERAL;
+                    candidate->value = first.value;
+                    break;
+                }
+
+                case lexer::token::TRUE:
+                {
+                    likely |= expr::BOOLEAN_OP_LIKELY; // Boolean operation is likely
+
+                    // Update the candidate
+                    candidate = allocator.alloc();
+                    candidate->rule = ast::TRUE;
+                    break;
+                }
+
+                case lexer::token::FALSE:
+                {
+                    // Update the candidate
+                    candidate = allocator.alloc();
+                    candidate->rule = ast::FALSE;
+                    break;
+                }
+
+                case lexer::token::OPEN_PAREN:
+                {
+                    likely |= expr::PROP_ACCESS_LIKELY; // Property access is likely
+
+                    // Handle nested expression
+                    candidate = allocator.alloc();
+                    candidate->rule = ast::EXPRESSION;
+
+                    // Extract the nested expression
+                    auto nested_expr = extract(expr_stream);
+                    expr_queue.emplace_back(container::move(nested_expr), candidate);
+                    expr_stream.next(); // Consume the close parenthesis
+                    break;
+                }
+
+                default:
+                {
+                    global_err.type = UNEXPECTED_TOKEN;
+                    global_err.column = trace.column;
+                    global_err.line = trace.line;
+                    throw except::exception("Unexpected token in expression");
+                    break;
+                }
+            }
+
+            // Start checking for likely ops
+            first_opt = expr_stream.next();
+            if (first_opt.is_none())
+            {
+                // Push the candidate to the current node
+                if (candidate != nullptr)
+                {
+                    parent->children.push_back(candidate);
+                    continue;
+                }
+
+                // Set error state
+                global_err.type = UNEXPECTED_TOKEN;
+                global_err.column = trace.column;
+                global_err.line = trace.line;
+                throw except::exception("Unexpected end of expression");
+            }
+
+            first = first_opt.get();
+            if (likely & expr::CALL_LIKELY && first.type == lexer::token::OPEN_PAREN)
+            {
+                // TODO!
+            }
+
+            if (likely & expr::PROP_ACCESS_LIKELY && first.type == lexer::token::DOT)
+            {
+                // TODO!
+            }
+
+            if (
+                likely & expr::BOOLEAN_OP_LIKELY
+                && (
+                    first.type == lexer::token::BOOL_EQ ||
+                    first.type == lexer::token::BOOL_GT ||
+                    first.type == lexer::token::BOOL_GTE ||
+                    first.type == lexer::token::BOOL_LT ||
+                    first.type == lexer::token::BOOL_LTE ||
+                    first.type == lexer::token::BOOL_NEQ
+                )
+            )
+            {
+                // TODO!
+            }
+
+            if (
+                likely & expr::ARITHMETIC_OP_LIKELY
+                && (
+                    first.type == lexer::token::PLUS ||
+                    first.type == lexer::token::MINUS ||
+                    first.type == lexer::token::MULTIPLY ||
+                    first.type == lexer::token::DIVIDE
+                )
+            )
+            {
+                // TODO!
+            }
+
+            // Start checking for likely ops
+            first_opt = expr_stream.next();
+            if (first_opt.is_none())
+            {
+                // Push the candidate to the current node
+                if (candidate != nullptr)
+                {
+                    parent->children.push_back(candidate);
+                    continue;
+                }
+
+                // Set error state
+                global_err.type = UNEXPECTED_TOKEN;
+                global_err.column = trace.column;
+                global_err.line = trace.line;
+                throw except::exception("Unexpected end of expression");
+            }
+
+            // Invalid token detected
+            global_err.type = UNEXPECTED_TOKEN;
+            global_err.column = trace.column;
+            global_err.line = trace.line;
+            throw except::exception("Unexpected token in expression");
+        }
+    }
+}
