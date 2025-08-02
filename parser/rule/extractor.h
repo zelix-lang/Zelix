1c736c984ad200a7e7e1f2e070acb9151294dc6e
rodrigoo-r <2nw53j32643y@gmail.com>
2025-08-01 20:22:38 -0500

    Add extractor function to handle token extraction with nested delimiters

---
+++ parser/rule/extractor.h
@@ -0,0 +1,109 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 8/1/25.
+//
+
+#pragma once
+#include "fluent/container/stream.h"
+#include "fluent/container/vector.h"
+#include "lexer/token.h"
+#include "parser/parser.h"
+
+namespace fluent::parser
+{
+    inline container::vector<lexer::token> extract(
+        container::stream<lexer::token> &tokens,
+        const lexer::token::t_type end_delim = lexer::token::OPEN_PAREN,
+        const lexer::token::t_type start_delim = lexer::token::OPEN_PAREN,
+        const bool handle_nested = true,
+        const bool exclude_first_delim = true
+    )
+    {
+        container::vector<lexer::token> result;
+        size_t nested_count = 0;
+
+        // Iterate over the tokens
+        auto current_opt = tokens.next();
+        while (current_opt.is_some())
+        {
+            const auto &current = current_opt.get();
+
+            if (current.type == end_delim)
+            {
+                // Handle nested delimiters
+                if (handle_nested)
+                {
+                    if (nested_count == 0)
+                    {
+                        global_err.type = UNEXPECTED_TOKEN;
+                        global_err.column = current.column;
+                        global_err.line = current.line;
+                        throw except::exception("Unexpected end delimiter");
+                    }
+
+                    nested_count--;
+                }
+
+                if (nested_count == 0)
+                {
+                    return result;
+                }
+            }
+            else if (current.type == start_delim)
+            {
+                if (handle_nested)
+                {
+                    nested_count++;
+                }
+
+                if (exclude_first_delim)
+                {
+                    current_opt = tokens.next();
+                    continue;
+                }
+            }
+
+            result.push_back(current);
+            current_opt = tokens.next();
+        }
+
+        // Delimiter never closed
+        if (current_opt.is_none())
+        {
+            global_err.type = UNEXPECTED_TOKEN;
+            global_err.column = 0;
+            global_err.line = 0;
+            throw except::exception("Unexpected end delimiter");
+        }
+
+        const auto &current = current_opt.get();
+        global_err.type = UNEXPECTED_TOKEN;
+        global_err.column = current.column;
+        global_err.line = current.line;
+        throw except::exception("Unexpected end delimiter");
+    }
+}
