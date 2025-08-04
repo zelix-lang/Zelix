dfde8a53f4ca11ee6d3ea1ee61ec00aaa0e510b6
Rodrigo R <141367232+rodrigoo-r@users.noreply.github.com>
2025-08-04 17:48:53 -0500

    Update expect function to handle pointer stream of tokens

--- parser/expect.h
+++ parser/expect.h
@@ -34,7 +34,7 @@
 namespace fluent::parser
 {
     inline void expect(
-        container::stream<lexer::token> &tokens,
+        container::stream<lexer::token *> &tokens,
         const lexer::token::t_type expected_type
     )
     {
@@ -42,7 +42,7 @@ namespace fluent::parser
         if (token_opt.is_none())
         {
             // Get the current token
-            container::optional<lexer::token> trace = tokens.curr();
+            container::optional<lexer::token *> trace = tokens.curr();
             if (trace.is_none())
             {
                 global_err.line = 0;
@@ -50,10 +50,10 @@ namespace fluent::parser
             }
             else
             {
-                auto current_token = trace.get();
+                const auto current_token = trace.get();
                 global_err.type = UNEXPECTED_TOKEN;
-                global_err.column = current_token.column; // Use the column from the current token
-                global_err.line = current_token.line; // Use the line from the current token
+                global_err.column = current_token->column; // Use the column from the current token
+                global_err.line = current_token->line; // Use the line from the current token
             }
 
             throw except::exception("Assertion failed");
@@ -63,12 +63,12 @@ namespace fluent::parser
         // Check if the token type matches the expected type
         if (
             const auto &token = token_opt.get();
-            token.type != expected_type
+            token->type != expected_type
         )
         {
             global_err.type = UNEXPECTED_TOKEN;
-            global_err.column = token.column; // Use the column from the token
-            global_err.line = token.line; // Use the line from the token
+            global_err.column = token->column; // Use the column from the token
+            global_err.line = token->line; // Use the line from the token
 
             // If it doesn't match, return false
             throw except::exception("Assertion failed");
