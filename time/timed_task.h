83ddb8c964e14dedcd698a6f7fd32c2982e36306
rodrigoo-r <2nw53j32643y@gmail.com>
2025-08-04 02:38:34 -0500

    Add timed_task.h for managing timed tasks in Fluent

---
+++ time/timed_task.h
@@ -0,0 +1,37 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 8/4/25.
+//
+
+#pragma once
+
+namespace fluent::time
+{
+    void post(const char *name, int max_steps);
+    void advance();
+    void complete(bool recompute_time = true);
+}
