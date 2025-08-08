43493c804c1f25b37c3956cb1ffa76447857b401
rodrigoo-r <github.com.dynamite518@passinbox.com>
2025-08-08 18:13:50 -0500

    Update build scripts to reflect the Zelix Programming Language branding and licensing

--- build.bat
+++ build.bat
@@ -1,15 +1,26 @@
 @echo off
-rem The Fluent Programming Language
-rem -----------------------------------------------------
-rem This code is released under the GNU GPL v3 license.
-rem For more information, please visit:
-rem https://www.gnu.org/licenses/gpl-3.0.html
-rem -----------------------------------------------------
-rem Copyright (c) 2025 Rodrigo R. & All Fluent Contributors
-rem This program comes with ABSOLUTELY NO WARRANTY.
-rem For details type `fluent l`. This is free software,
-rem and you are welcome to redistribute it under certain
-rem conditions; type `fluent l -f` for details.
+rem         ==== The Zelix Programming Language ====
+rem    ---------------------------------------------------------
+rem      - This file is part of the Zelix Programming Language
+rem        codebase. Zelix is a fast, statically-typed and
+rem        memory-safe programming language that aims to
+rem        match native speeds while staying highly performant.
+rem    ---------------------------------------------------------
+rem      - Zelix is categorized as free software; you can
+rem        redistribute it and/or modify it under the terms of
+rem        the GNU General Public License as published by the
+rem        Free Software Foundation, either version 3 of the
+rem        License, or (at your option) any later version.
+rem    ---------------------------------------------------------
+rem      - Zelix is distributed in the hope that it will
+rem        be useful, but WITHOUT ANY WARRANTY; without even
+rem        the implied warranty of MERCHANTABILITY or FITNESS
+rem        FOR A PARTICULAR PURPOSE. See the GNU General Public
+rem        License for more details.
+rem    ---------------------------------------------------------
+rem      - You should have received a copy of the GNU General
+rem        Public License along with Zelix. If not, see
+rem        <https://www.gnu.org/licenses/>.
 
 if not exist "cmake-build-debug" (
     mkdir cmake-build-debug
--- build.sh
+++ build.sh
@@ -1,14 +1,25 @@
-#    The Fluent Programming Language
-#    -----------------------------------------------------
-#    This code is released under the GNU GPL v3 license.
-#    For more information, please visit:
-#    https://www.gnu.org/licenses/gpl-3.0.html
-#    -----------------------------------------------------
-#    Copyright (c) 2025 Rodrigo R. & All Fluent Contributors
-#    This program comes with ABSOLUTELY NO WARRANTY.
-#    For details type `fluent l`. This is free software,
-#    and you are welcome to redistribute it under certain
-#    conditions; type `fluent l -f` for details.
+#        ==== The Zelix Programming Language ====
+#---------------------------------------------------------
+#  - This file is part of the Zelix Programming Language
+#    codebase. Zelix is a fast, statically-typed and
+#    memory-safe programming language that aims to
+#    match native speeds while staying highly performant.
+#---------------------------------------------------------
+#  - Zelix is categorized as free software; you can
+#    redistribute it and/or modify it under the terms of
+#    the GNU General Public License as published by the
+#    Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#---------------------------------------------------------
+#  - Zelix is distributed in the hope that it will
+#    be useful, but WITHOUT ANY WARRANTY; without even
+#    the implied warranty of MERCHANTABILITY or FITNESS
+#    FOR A PARTICULAR PURPOSE. See the GNU General Public
+#    License for more details.
+#---------------------------------------------------------
+#  - You should have received a copy of the GNU General
+#    Public License along with Zelix. If not, see
+#    <https://www.gnu.org/licenses/>.
 
 if [ ! -d "cmake-build-debug" ]; then
     mkdir cmake-build-debug
