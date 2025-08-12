7e8cea93cb62bfb31a2db6973c47299a80ac559b
rodrigoo-r <github.com.dynamite518@passinbox.com>
2025-08-11 20:00:09 -0500

    Add header files for declaration, file_code, function, mod, and type structures

---
+++ file_code/file_code.h
@@ -0,0 +1,52 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Zelix is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Zelix. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodri on 8/10/25.
+//
+
+#pragma once
+#include "ankerl/unordered_dense.h"
+#include "function.h"
+#include "mod.h"
+#include "parser/ast.h"
+
+namespace zelix::code
+{
+    struct file_code
+    {
+        ankerl::unordered_dense::map<
+            container::external_string,
+            function *,
+            container::external_string_hash
+        > functions; // The function map
+
+        ankerl::unordered_dense::map<
+            container::external_string,
+            mod *,
+            container::external_string_hash
+        > modules; // The function map
+    };
+}
---
+++ file_code/function.h
@@ -0,0 +1,46 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Zelix is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Zelix. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodri on 8/10/25.
+//
+
+#pragma once
+#include "parser/ast.h"
+#include "type.h"
+
+namespace zelix::code
+{
+    struct function
+    {
+        type *return_type;
+        ankerl::unordered_dense::map<
+            container::external_string,
+            type *,
+            container::external_string_hash
+        > args; // The function's arguments
+        parser::ast *body = nullptr; // The function's body
+    };
+}
