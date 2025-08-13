8e69cc16be9f513903c5dca3f9941522c6b6fa58
rodrigoo-r <github.com.dynamite518@passinbox.com>
2025-08-12 19:14:24 -0500

    Add constants header file for Zelix programming language

---
+++ global/constants.h
@@ -0,0 +1,45 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Zelix is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Zelix. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodri on 8/12/25.
+//
+
+#pragma once
+#include <cstdlib>
+#include <zelix/container/owned_string.h>
+
+namespace zelix::constants
+{
+    inline auto user_home = container::string(
+#       ifdef _WIN32
+        getenv("USERPROFILE")
+#       else
+        getenv("HOME")
+#       endif
+    );t
+
+    inline auto stdlib = user_home + "/.zelix/stdlib";
+}
