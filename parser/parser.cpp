2cfba3ab10aecd2850123eeaeb166e16ad6642c6
rodrigoo-r <rodrigo@zelixlang.dev>
2025-08-16 13:02:37 -0500

    Update import path in parser.cpp for improved file structure consistency

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -28,7 +28,7 @@
 #include "memory/allocator.h"
 #include "rule/function/function.h"
 #include "rule/import/import.h"
-#include "rule/mod.h"
+#include "rule/mod/mod.h"
 #include "rule/package.h"
 using namespace zelix;
 
