1c3b4058dea1c656bc3eb04ba92d85ecff72f7b2
rodrigoo-r <rodrigo@zelixlang.dev>
2025-08-16 13:08:27 -0500

    Refactor package function in package.h for improved readability and structure

--- parser/rule/package/package.h
+++ parser/rule/package/package.h
@@ -31,112 +31,15 @@
 
 #include "memory/allocator.h"
 #include "parser/ast.h"
-#include "parser/expect.h"
 #include "zelix/container/stream.h"
+#include "lexer/token.h"
 
 namespace zelix::parser::rule
 {
     template <bool Expect, lexer::token::t_type Until, bool IsType = false>
-    inline void package(
+    void package(
         ast *&root,
         container::stream<lexer::token *> &tokens,
         memory::lazy_allocator<ast> &allocator
-    )
-    {
-        if constexpr (Expect)
-        {
-            // Expect the very first token to be a package
-            expect(tokens, lexer::token::PACKAGE);
-        }
-
-        lexer::token *trace;
-        if constexpr (Expect)
-        {
-            trace = tokens.next().get(); // Consume the package token
-        }
-        else
-        {
-            trace = tokens.curr().get();
-        }
-
-        bool id = true; // Flag to track if we are expecting an identifier
-
-        // Allocate a new AST node for the package
-        ast *package_node = allocator.alloc();
-        package_node->rule = ast::PACKAGE;
-        root->children.push_back(package_node); // Add the package node to the root
-
-        // Get the next token
-        auto next_opt = tokens.peek();
-        if (next_opt.is_none())
-        {
-            global_err.type = UNEXPECTED_TOKEN;
-            global_err.column = trace->column;
-            global_err.line = trace->line;
-            throw except::exception("Unexpected end of input while parsing package");
-        }
-
-        while (next_opt.is_some())
-        {
-            const auto &next = next_opt.get();
-
-            // Break if we have reached the Until token
-            if (next->type == Until)
-            {
-                if constexpr (!IsType)
-                {
-                    tokens.next(); // Consume the token
-                }
-
-                break;
-            }
-
-            if (id)
-            {
-                if (next->type != lexer::token::IDENTIFIER)
-                {
-                    return; // Break if we are not expecting an identifier
-                }
-
-                tokens.next(); // Consume the identifier token
-                // Allocate a new AST node for the identifier
-                ast *id_node = allocator.alloc();
-                id_node->rule = ast::IDENTIFIER;
-                id_node->value = next->value; // Set the identifier value
-                id_node->line = next->line; // Set the line number
-                id_node->column = next->column; // Set the column number
-                package_node->children.push_back(id_node); // Add the identifier node to the package node
-                id = false; // Switch to expecting a dot next
-                next_opt = tokens.peek(); // Get the next token
-                continue;
-            }
-
-            if (next->type != lexer::token::DOT)
-            {
-                if constexpr (IsType)
-                {
-                    // Break for the type parser
-                    break;
-                }
-
-                global_err.type = UNEXPECTED_TOKEN;
-                global_err.column = next->column;
-                global_err.line = next->line;
-                throw except::exception("Expected '.' after package identifier");
-            }
-
-            tokens.next(); // Consume the dot token
-            id = true; // Switch to expecting an identifier next
-            next_opt = tokens.peek(); // Get the next token
-        }
-
-        // Make sure we don't end up expecting an identifier
-        if (id)
-        {
-            global_err.type = UNEXPECTED_TOKEN;
-            global_err.column = trace->column;
-            global_err.line = trace->line;
-            throw except::exception("Unexpected end of input while parsing package");
-        }
-    }
+    );
 }
