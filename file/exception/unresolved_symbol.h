7ecbf9b26fabedd817f25c7e63a8d90a2db36ceb
rodrigoo-r <rodrigo@zelixlang.dev>
2025-08-15 23:36:26 -0500

    Add unresolved_symbol exception class for error handling

---
+++ file/exception/unresolved_symbol.h
@@ -0,0 +1,41 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Zelix is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Zelix. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodri on 8/15/25.
+//
+
+#pragma once
+#include "zelix/except/exception.h"
+
+namespace zelix::code::exception
+{
+    class unresolved_symbol final : except::exception
+    {
+        explicit unresolved_symbol(const char *msg)
+            : exception(msg)
+        {}
+    };
+}
