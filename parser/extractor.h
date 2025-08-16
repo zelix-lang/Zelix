d948799b6b2783647d9664d5ddae67733c13d8c8
rodrigoo-r <rodrigo@zelixlang.dev>
2025-08-16 11:38:35 -0500

    Refactor extract function signature for improved readability

--- parser/extractor.h
+++ parser/extractor.h
@@ -28,105 +28,15 @@
 //
 
 #pragma once
-#include "zelix/container/stream.h"
-#include "lexer/token.h"
-#include "parser/parser.h"
 
 namespace zelix::parser
 {
-    inline container::stream<lexer::token *> extract(
+    container::stream<lexer::token *> extract(
         container::stream<lexer::token*> &tokens,
-        const lexer::token::t_type end_delim = lexer::token::CLOSE_PAREN,
-        const lexer::token::t_type nested_end_delim = lexer::token::CLOSE_PAREN,
-        const lexer::token::t_type start_delim = lexer::token::OPEN_PAREN,
-        const bool handle_nested = true,
-        const bool exclude_first_delim = true
-    )
-    {
-        container::vector<lexer::token *> vec;
-        container::stream result(container::move(vec));
-        size_t nested_count = 0;
-        const size_t start_pos = tokens.pos();
-
-        // Iterate over the tokens
-        auto next_opt = tokens.next();
-        while (next_opt.is_some())
-        {
-            const auto &current = next_opt.get();
-
-            if (current->type == nested_end_delim)
-            {
-                // Handle nested delimiters
-                if (handle_nested)
-                {
-                    if (nested_count == 0)
-                    {
-                        tokens.set_pos(start_pos);
-                        global_err.type = UNEXPECTED_TOKEN;
-                        global_err.column = current->column;
-                        global_err.line = current->line;
-                        throw except::exception("Unexpected nested end delimiter");
-                    }
-
-                    nested_count--;
-
-                    if (nested_count == 0 && nested_end_delim == end_delim)
-                    {
-                        return result;
-                    }
-                }
-            }
-
-            else if (current->type == end_delim)
-            {
-                // Handle nested delimiters
-                if (handle_nested)
-                {
-                    if (nested_count != 0)
-                    {
-                        result.push(current);
-                        next_opt = tokens.next();
-                        continue; // Continue to the next token
-                    }
-                }
-
-                return result;
-            }
-
-            if (current->type == start_delim)
-            {
-                if (handle_nested)
-                {
-                    nested_count++;
-                }
-
-                if (exclude_first_delim)
-                {
-                    next_opt = tokens.next();
-                    continue;
-                }
-            }
-
-            result.push(current);
-            next_opt = tokens.next();
-        }
-
-        // Restore the original position of the tokens stream
-        tokens.set_pos(start_pos);
-
-        // Delimiter never closed
-        if (next_opt.is_none())
-        {
-            global_err.type = UNEXPECTED_TOKEN;
-            global_err.column = 0;
-            global_err.line = 0;
-            throw except::exception("Unexpected end delimiter");
-        }
-
-        const auto &current = next_opt.get();
-        global_err.type = UNEXPECTED_TOKEN;
-        global_err.column = current->column;
-        global_err.line = current->line;
-        throw except::exception("Unexpected end delimiter");
-    }
+        lexer::token::t_type end_delim = lexer::token::CLOSE_PAREN,
+        lexer::token::t_type nested_end_delim = lexer::token::CLOSE_PAREN,
+        lexer::token::t_type start_delim = lexer::token::OPEN_PAREN,
+        bool handle_nested = true,
+        bool exclude_first_delim = true
+    );
 }
